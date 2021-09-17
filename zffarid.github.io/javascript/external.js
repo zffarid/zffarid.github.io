de968432afe7d94b8711339f3a3fe3ee12317fd6
Zainab Farid <zainabfarid05@gmail.com>
2021-09-17 14:38:28 -0400

    Completed Task 5. Conditional Statement.

--- zffarid.github.io/javascript/external.js
+++ zffarid.github.io/javascript/external.js
@@ -13,8 +13,22 @@
 //sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
 //sign = window.prompt('Are you feeling lucky', 'sure'); //open the window with Text "Are you feeling lucky" and default value "sure"
 
-x = 22;
-const y =33;
-document.writeln(x + y);
-document.writeln("<br>");
-document.writeln(x += 33);
+//x = 22;
+//y =33;
+//document.writeln(x + y);
+//document.writeln("<br>");
+//document.writeln(x += 33);
+
+document.writeln("<button onclick='condition()'>Conditional Test</button>")
+function condition()
+{
+ x = confirm("Are you sure you want to proceed?");
+ if(x)
+ {
+ document.writeln("You chose Okay!");
+ }
+ else
+ {
+ document.writeln("You chose Cancel!");
+ }
+}
