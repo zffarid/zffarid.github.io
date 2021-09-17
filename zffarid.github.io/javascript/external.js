6a71eec04dce44e7aa8145e8d4efd5ac2364a77b
Zainab Farid <zainabfarid05@gmail.com>
2021-09-17 11:30:10 -0400

    Completed Task 4. Operators.

--- zffarid.github.io/javascript/external.js
+++ zffarid.github.io/javascript/external.js
@@ -1,14 +1,20 @@
-// document.write("Where is my JS code?"); //
-//const x = "web";//
-//const y = "mapping";//
-//const out = x + y;//
-//document.write(out)//
+// document.write("Where is my JS code?");
+//const x = "web";
+//const y = "mapping";
+//const out = x + y;
+//document.write(out)
 
 //A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt('Please enter your name', 'Type your name here');
-document.write(user_name);
+//user_name = window.prompt('Please enter your name', 'Type your name here');
+//document.write(user_name);
 // there are many ways to use the prompt feature
-sign = window.prompt(); // open the blank prompt window
-sign = prompt(); // open the blank prompt window
-sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
-sign = window.prompt('Are you feeling lucky', 'sure'); //open the window with Text "Are you feeling lucky" and default value "sure"
+//sign = window.prompt(); // open the blank prompt window
+//sign = prompt(); // open the blank prompt window
+//sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+//sign = window.prompt('Are you feeling lucky', 'sure'); //open the window with Text "Are you feeling lucky" and default value "sure"
+
+x = 22;
+const y =33;
+document.writeln(x + y);
+document.writeln("<br>");
+document.writeln(x += 33);
