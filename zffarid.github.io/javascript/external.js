4a52e062e93bec10dc30bc3785834c98cbd64b56
Zainab Farid <zainabfarid05@gmail.com>
2021-09-17 11:27:54 -0400

    Completed Task 3. Variables.

--- zffarid.github.io/javascript/external.js
+++ zffarid.github.io/javascript/external.js
@@ -1,4 +1,14 @@
 // document.write("Where is my JS code?"); //
-window.alert(5 + 6);
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+//const x = "web";//
+//const y = "mapping";//
+//const out = x + y;//
+//document.write(out)//
+
+//A prompt box is used to prompt users to input a value before entering a page.
+user_name = window.prompt('Please enter your name', 'Type your name here');
+document.write(user_name);
+// there are many ways to use the prompt feature
+sign = window.prompt(); // open the blank prompt window
+sign = prompt(); // open the blank prompt window
+sign = window.prompt('Are you feeling lucky'); // open the window with Text "Are you feeling lucky"
+sign = window.prompt('Are you feeling lucky', 'sure'); //open the window with Text "Are you feeling lucky" and default value "sure"
