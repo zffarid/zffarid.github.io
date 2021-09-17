f5ccb3ffa85cb5d4981f48b606250c1b7d0214f7
Zainab Farid <zainabfarid05@gmail.com>
2021-09-17 11:08:35 -0400

    Completed Task 2. Output.

--- zffarid.github.io/javascript/external.js
+++ zffarid.github.io/javascript/external.js
@@ -1,1 +1,4 @@
-document.write("Where is my JS code?");
+// document.write("Where is my JS code?"); //
+window.alert(5 + 6);
+document.write('<h1>A heading</h1>');
+document.write('<p>A sentence.</p>');
