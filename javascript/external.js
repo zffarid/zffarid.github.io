dd2836147aa85a9e81838f182bcd79b5400f42a8
Zainab Farid <zainabfarid05@gmail.com>
2021-10-07 04:18:45 -0400

    Defined a function for the welcome message

--- javascript/external.js
+++ javascript/external.js
@@ -37,6 +37,20 @@
 // for (var i=0; i < arr.length; i++)
 // {
 //  document.writeln(arr[i],"<br>");
-// }
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
+// // }
+// user_name = window.prompt("Please enter your name", "Type your name here");
+// document.write(user_name);
+
+function welcome()
+{
+  let a = "Please enter your name.";
+  let b = "Type your name here";
+
+  user_name = window.prompt(a,b);
+
+  message = "<h1>Hello, welcome to my webpage," + user_name + "!</h1>"
+
+  return message
+}
+
+document.write(welcome());
