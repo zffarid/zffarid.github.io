8070c1441b0ca20f0a170d2f70def63b7786eaf4
Zainab Farid <zainabfarid05@gmail.com>
2021-10-08 01:04:56 -0400

    Filled content from web maps array into the web maps table.

--- javascript/external.js
+++ javascript/external.js
@@ -52,3 +52,30 @@ function welcome()
 
   return message
 }
+
+var webmaps =
+[
+  ["Migrations in Motion",
+  "http://maps.tnc.org/migrations-in-motion/#4/19.00/-78.00/",
+  "The Migrations in Motion map by the Nature Conservancy that shows the average direction mammals, birds, and amphibians need to move to track hospitable climates as they shift across the landscape"
+],
+["The Food Capitals of Instagram",
+"https://www.cewe.co.uk/instagram-food-capitals.html/",
+"The Food Capitals of Instagram map shows the location of photos with food hashtags obtained from Instagram and includes locations of over 10,000 photos posted on Instagram with relevant tags betwen 10th and 18th of March 2015"
+  ]
+];
+
+function webmap_table()
+{
+  document.write("<table width=100%>");
+  for (var row=0; row < webmaps.length; row++)
+  {
+    document.write("<tr>");
+    for (var column=0; column < webmaps[0].length; column++)
+    {
+      document.write("<td>" + webmaps[row][column] + "</td>");
+    }
+    document.write("</tr>");
+  }
+  document.write("</table>");
+}
